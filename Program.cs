6511f189b7660a926d485de40988200d1e1d1212
Sader Fawall <sader1992@hotmail.com>
2023-04-19 00:25:30 +0200

    re-optmizing the whole code.

--- Program.cs
+++ Program.cs
@@ -8,39 +8,27 @@ namespace Sader_Ragnarok_Login_Background
     {
         static void Main(string[] args)
         {
-            if (args.Length == 0)
+            //Check if the user has provided an image file as an argument
+            if (args.Length == 0 || !File.Exists(args[0]))
             {
                 Console.WriteLine("Please drag and drop the image you want on the application!");
                 Console.WriteLine("When you drag and drop the image onto the application the folder will be created in the same path.");
+                Console.WriteLine("also you will get this error if the file doesn't exist.");
                 Console.WriteLine("Press any key to exit...");
                 Console.ReadLine();
-                Environment.Exit(0);
+                return;
             }
 
-            string file = "";
+            string file = args[0];
 
-            try
-            {
-                file = args[0];
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Something went wrong!!");
-                Console.WriteLine("please make sure there is no odd character in the file path or file name!");
-                Console.WriteLine("File: " + file);
-                Console.WriteLine("Press any key to exit...");
-                Console.ReadLine();
-                Environment.Exit(0);
-            }
-
+            //Check if the file extension is valid
             if (Path.GetExtension(file) != ".jpg" && Path.GetExtension(file) != ".bmp" && Path.GetExtension(file) != ".png")
             {
                 Console.WriteLine("the image extension must be .jpg , .bmp or .png");
                 Console.WriteLine("Press any key to exit...");
                 Console.ReadLine();
-                Environment.Exit(0);
+                return;
             }
-            string path = Path.GetDirectoryName(file);
 
             Image img = Image.FromFile(file);
             Console.WriteLine("What is the number of the login image ? (normally it's Nothing or 2)");
@@ -48,64 +36,70 @@ namespace Sader_Ragnarok_Login_Background
             string n = Console.ReadLine();
             Bitmap src = new Bitmap(img, new Size(256 * 4, 256 * 3));
 
-            string[] name = {
-                    "t" + n + "_배경1-1",
-                    "t" + n + "_배경1-2",
-                    "t" + n + "_배경1-3",
-                    "t" + n + "_배경1-4",
-
-                    "t" + n + "_배경2-1",
-                    "t" + n + "_배경2-2",
-                    "t" + n + "_배경2-3",
-                    "t" + n + "_배경2-4",
-
-                    "t" + n + "_배경3-1",
-                    "t" + n + "_배경3-2",
-                    "t" + n + "_배경3-3",
-                    "t" + n + "_배경3-4"
-            };
-
-            int[] dst = {
-                0,0,
-                256,0,
-                512,0,
-                768,0,
-
-                0,256,
-                256,256,
-                512,256,
-                768,256,
-
-                0,512,
-                256,512,
-                512,512,
-                768,512,
-            };
-
-            if (!Directory.Exists(@"data"))
-            {
-                Directory.CreateDirectory(@"data");
-            }
-            if (!Directory.Exists(@"data\texture"))
-            {
-                Directory.CreateDirectory(@"data\texture");
-            }
-            if (!Directory.Exists(@"data\texture\유저인터페이스"))
-            {
-                Directory.CreateDirectory(@"data\texture\유저인터페이스");
-            }
-
-            int d = 0;
+            //Using a loop to generate the name array
+            string[] name = new string[12];
             for (int i = 0; i < 12; i++)
             {
-                Rectangle cropRect = new Rectangle(dst[d++], dst[d++], 256, 256);
+                int row = i / 4 + 1;
+                int col = i % 4 + 1;
+                name[i] = $"t{n}_배경{row}-{col}";
+            }
+
+            //Create a two-dimensional array to store the destination coordinates
+            int[,] dst = {
+                {0,0},
+                {256,0},
+                {512,0},
+                {768,0},
+
+                {0,256},
+                {256,256},
+                {512,256},
+                {768,256},
+
+                {0,512},
+                {256,512},
+                {512,512},
+                {768,512},
+            };
+
+            //Create the directory and all its subdirectories if they do not exist
+            try
+            {
+                // Try to create the directory and all its subdirectories
+                Directory.CreateDirectory(@"data\texture\유저인터페이스");
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                // Handle the case when the caller does not have the required permission
+                Console.WriteLine("You do not have the permission to create the directory.");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception ex)
+            {
+                // Handle the case when the directory cannot be created due to anything other than the permissions.
+                Console.WriteLine("There was a problem creating the directory.");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadLine();
+                return;
+            }
+
+            //Access the destination coordinates from the two-dimensional array
+            for (int i = 0; i < 12; i++)
+            {
+                Rectangle cropRect = new Rectangle(dst[i, 0], dst[i, 1], 256, 256);
                 Bitmap target = new Bitmap(cropRect.Width, cropRect.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
 
                 using (Graphics g = Graphics.FromImage(target))
                     g.DrawImage(src, new Rectangle(0, 0, target.Width, target.Height), cropRect, GraphicsUnit.Pixel);
-                target.Save(@"data\texture\유저인터페이스\" + name[i] + ".bmp",System.Drawing.Imaging.ImageFormat.Bmp);
+                target.Save($@"data\texture\유저인터페이스\{name[i]}.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
             }
-            Console.WriteLine("Done, next to the image you will find a data folder , add it to your Grf.");
+
+            Console.WriteLine("Done, next to the image you will find a data folder , add it to your '.grf'.");
             Console.WriteLine("Press any key to exit...");
             Console.ReadLine();
         }
